2a15b9f255a4f35efc7ef316c4bbc6cf4c8cbb63
Codeneir <metatext1@gmail.com>
2015-04-17 18:32:53 +0300

    For_version_for_exception
    For-cycle

--- Second_program.py
+++ Second_program.py
@@ -1,12 +1,14 @@
 def repeater(interfunction):
-    try:
-        interfunction();
-    except Exception:
-        try:
+    i = 0L;
+    for i in [1,2,3]:
+        if i != 3:
+            try:
+                interfunction();
+            except:
+                pass;
+        else:
             interfunction();
-        except Exception:
-            interfunction();
-                
+            
 @repeater
 def hello():
     print 'Hello!';
