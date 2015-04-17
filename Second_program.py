a4c60807b23546fd46c89cdc351e1936054730c2
Codeneir <metatext1@gmail.com>
2015-04-17 19:15:45 +0300

    Finally

--- Second_program.py
+++ Second_program.py
@@ -1,16 +1,15 @@
 def notify(interfunction):
     def catcher():
         for i in range(3):
-            if i != 3:
-                try:
-                    interfunction();
-                except:
-                    pass
+            try:
+                interfunction();
+            except:
+                pass
     return catcher();
 
 @notify
 def halloer():
     print 'hello!';
-    Exception;
+    raise Exception;
 
 halloer();              
